3068a5fb5d592fd0fa2b688cff47b4d33ef36338
Midoooox <m_ghasmoune@estin.dz>
2025-05-29 23:00:20 +0100

    Update settings.py

--- stins/settings.py
+++ stins/settings.py
@@ -1,6 +1,7 @@
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -71,10 +72,7 @@ WSGI_APPLICATION = 'stins.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': dj_database_url.config(default='db.sqlite3')
 }
 
 # Password validation
